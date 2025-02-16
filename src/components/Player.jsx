f83c2a935176e9854f756d46d411c513a694bb0f
Magno <jmagno.dutra@hotmail.com>
2025-02-15 22:56:49 -0300

    criado player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+  const [submited, setSubmitted] = useState(false);
+
+  function handleChange(e) {
+    setSubmitted(false);
+    setEnteredPlayerName(e.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submited ? enteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
+        <input type="text" value={enteredPlayerName} onChange={handleChange} />
         <button>Set Name</button>
       </p>
     </section>
